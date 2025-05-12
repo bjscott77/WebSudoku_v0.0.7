f0f73fffc175d061284dc9decfe542ffef68b508
bjscott77 <bjscott77@gmail.com>
2025-05-12 15:05:38 -0500

    entity base

--- WebSudoku_v0.0.7/WebSudoku_v0.0.7/Models/EntityBase.cs
+++ WebSudoku_v0.0.7/WebSudoku_v0.0.7/Models/EntityBase.cs
@@ -1,8 +1,32 @@
-﻿namespace WebSudoku_v0._0._7.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebSudoku_v0._0._7.Models
 {
     public abstract class EntityBase : IDisposable
     {
-        public Guid Id { get; set; }
+        public Guid Id { get; internal set; }
+
+        public EntityBase()
+        {
+            this.Id = Guid.NewGuid();
+        }
+        public EntityBase(Guid id)
+        {
+            this.Id = id;
+        }
+        public EntityBase(string id)
+        {
+            this.Id = Guid.Parse(id);
+        }
+        public EntityBase(EntityBase entityBase)
+        {
+            this.Id = entityBase.Id;
+        }
+
+        public string GetIdAsString()
+        {
+            return this.Id.ToString();
+        }
 
         public void Dispose()
         {
